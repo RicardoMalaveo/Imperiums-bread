285260c103bc6e915ee497da0093247d74a42b41
RICARDOS-LAPTOP\Ricardo Malavé <ricardomalaveo@gmail.com>
2025-02-27 14:33:29 +0100

    movemente is set for the player, working on the dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //movemente variables
+    public Rigidbody ricktusRB;
+    public float movementSpeed;
+    public float staminaLevel = 1;
+    private Vector2 movementInput;
+
+    //Dash variables
+
+    public float dashSpeed = 15F;
+    public float dashLength = 0.5F;
+    public float dashBufferLength = 0.1F;
+    private float dashBufferCounter;
+    private bool isDashing;
+    private bool hasDashed;
+    private bool canDash => dashBufferCounter > 0F && !hasDashed;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +29,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        movementInput.x = Input.GetAxis("Horizontal");
+        movementInput.y = Input.GetAxis("Vertical");
+        //movementInput.Normalize();
+
+        ricktusRB.linearVelocity = new Vector3(movementInput.x * movementSpeed, ricktusRB.linearVelocity.y, movementInput.y * movementSpeed);
+
         
+
     }
 }
